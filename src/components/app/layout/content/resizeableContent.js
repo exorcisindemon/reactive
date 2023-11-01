3040dcb1d840f81fd7170cdfff60a6ddf147f535
sievl <sievl@outlook.com>
2023-11-01 14:58:35 +0700

    update

--- src/components/app/layout/content/resizeableContent.js
+++ src/components/app/layout/content/resizeableContent.js
@@ -1,20 +1,28 @@
 import { TbUserCircle } from "react-icons/tb";
 
-const ResizeableContent = () => {
+const ResizeableContent = ({caption}) => {
   return (
-    <main className="bg-white flex flex-col w-full lg:w-[480px] h-auto lg:h-[480px] p-8 rounded-2xl">
+    <main className="bg-white flex flex-col w-full lg:w-[480px] h-auto p-8 rounded-2xl">
       <div className="flex flex-col lg:flex-row gap-4 items-center">
         <div className="p-4 ring-1 ring-zinc-200 rounded-full">
           <TbUserCircle />
         </div>
         <div className="flex flex-col items-center lg:items-start">
-          <p className="text-black font-medium text-sm">sievl</p>
-          <p className="text-black font-normal text-xs">
+          <p className="text-black font-medium text-sm mt-0">sievl</p>
+          <p className="text-zinc-600 font-normal text-xs mt-0">
             Semarang, Central Java, Indonesia
           </p>
         </div>
       </div>
-      <div className="bg-zinc-200 flex flex-col w-full h-full p-4 lg:p-8 rounded-2xl mt-8"></div>
+      <div className="bg-zinc-200 flex flex-col w-full h-full p-4 lg:p-8 rounded-2xl mt-8">
+        &resizeableContent
+      </div>
+      <p className="text-zinc-600 font-normal text-xs mt-8">
+        {caption}
+      </p>
+      <p className="text-black font-normal text-xs mt-8">
+        Wednesday, November 1, 2023
+      </p>
     </main>
   );
 };
